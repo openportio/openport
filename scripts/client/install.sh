e5fa1e3102e2e2baed3cda5b25035a6a44c58a7d
Jan De Bleser <jan@jan-VirtualBox.(none)>
2013-05-20 17:54:36 +0200

    adding hashbangs and fix for symlinks

--- scripts/client/install.sh
+++ scripts/client/install.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 sudo apt-get install python-pip python-virtualenv python-dev libsqlite3-dev
 virtualenv env
 env/bin/pip install -r requirements.pip
 
-ln -s $(dirname $1)/openport-tray /etc/init.d/openport-tray
-update-rc.d openport-tray defaults
+sudo ln -s $(dirname $1)/openport-tray /etc/init.d/openport-tray
+sudo update-rc.d openport-tray defaults
--- scripts/client/openport.sh
+++ scripts/client/openport.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+cd $(dirname $(readlink $0))
 env/bin/python apps/openport_app.py --local-port $1 --no-gui
