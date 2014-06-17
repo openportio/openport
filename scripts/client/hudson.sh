3958f449679d9f18fc6b61e286015e2ffa652503
Jan De Bleser <jandebleser@gmail.com>
2014-06-17 08:45:25 +0200

    Moved hudson script to separate file

--- scripts/client/hudson.sh
+++ scripts/client/hudson.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 cd scripts/client
 virtualenv env
-#sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount gcc libsqlite3-dev
-#mysql -uroot -e "grant all privileges on test_openthegate.* to 'hudson'@'localhost';" -p
-env/bin/easy_install -U distribute
-env/bin/easy_install unittest-xml-reporting
+#sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount libffi-dev
+#mysql -uroot -e "grant all privileges on test_openport.* to 'hudson'@'localhost';" -p
+#env/bin/easy_install -U distribute
+#env/bin/easy_install unittest-xml-reporting
 env/bin/pip install -r requirements.pip
-env/bin/python tests/integrationtests.py
+env/bin/unit2 discover tests "*_tests.py"
