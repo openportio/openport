5e993c7780266e9449ea941aa37c36b765931d68
Jan De Bleser <jandebleser@gmail.com>
2014-06-17 09:32:37 +0200

    Made the tests a bit more robust

--- scripts/client/tests/integration_tests.py
+++ scripts/client/tests/integration_tests.py
@@ -39,7 +39,8 @@ class IntegrationTest(unittest.TestCase):
         share = self.get_share(path)
         self.start_sharing(share)
         temp_file = os.path.join(os.path.dirname(__file__), os.path.basename(share.filePath))
-        sleep(1)
+        sleep(5)
+        print 'temp file: ' + temp_file
         self.downloadAndCheckFile(share, temp_file)
 
     def get_share(self, path):
