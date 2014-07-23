2a910ddc1486ed4fff6b42e2addae8820f14e3ac
Jan De Bleser @ Virtualbox-64 <jandebleser@gmail.com>
2014-07-23 16:59:08 +0200

    updated installation script

--- scripts/client/create_exes.sh
+++ scripts/client/create_exes.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 env/bin/pyinstaller apps/openport_app.py --clean --onefile --name openport
+
+
+
+#If the exe fails with "cannot import _counter":
+# wget https://github.com/pyinstaller/pyinstaller/tarball/develop
+# tar -xf develop
+# cd pyinstaller-pyinstaller-*
+# ../env/bin/python setup.py
+# cd ..
+# And try again
--- scripts/client/install.sh
+++ scripts/client/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#sudo apt-get install python-pip python-virtualenv python-dev libsqlite3-dev phantomjs libffi-dev
+sudo apt-get install -y python-pip python-virtualenv python-dev libsqlite3-dev phantomjs libffi-dev libssl-dev
 # brew install phantomjs
 virtualenv env
 env/bin/pip install -r requirements.pip
 
-sudo ln -s -f $(pwd)/openport-manager /etc/init.d/openport-manager
-sudo update-rc.d openport-manager defaults
+#sudo ln -s -f $(pwd)/openport-manager /etc/init.d/openport-manager
+#sudo update-rc.d openport-manager defaults
