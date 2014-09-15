681da517e2ca7e4aa4432f4f03058e5f237ab7d1
Jan De Bleser <jandebleser@gmail.com>
2014-09-15 18:07:50 +0200

    fixed installation scripts for debian package

--- scripts/createdeb.sh
+++ scripts/createdeb.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-VERSION=2.0.0
+VERSION=2.0.1
 
 APPLICATION=openport
 PACKAGE=openport-$VERSION
 TARBALL=openport_$VERSION.orig.tar.gz
 
-sudo apt-get --yes install build-essential autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian pbuilder python-dev python-pip python-virtualenv libsqlite3-dev
+# sudo apt-get --yes install build-essential autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian pbuilder python-dev python-pip python-virtualenv libsqlite3-dev
 
 # if you have errors from locale: sudo dpkg-reconfigure locales
+
+sudo dpkg --remove $APPLICATION || echo "$APPLICATION not installed"
 
 rm -rf $PACKAGE
 mkdir $PACKAGE
 mkdir -p $PACKAGE/usr/bin
-#mkdir -p $PACKAGE/etc/init.d
 cp client/dist/* $PACKAGE/usr/bin/ -r
-cp client/openport.startup $PACKAGE/etc/init.d/openport-manager.startup
-#rm $PACKAGE/client/env -rf
+cp client/openport.startup debian/openport.init
 tar -czf $TARBALL $PACKAGE
 rm -rf $PACKAGE
 rm -rf package
@@ -36,4 +36,4 @@ cd ../..
 sudo rm -rf /usr/bin/openport
 killall python
 sudo dpkg -i package/openport_$(echo $VERSION)-1_*.deb
-openport
+openport -h
