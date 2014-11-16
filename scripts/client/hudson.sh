ab2cf6f6e41f4b7dcf2ab955ce0720636ce36d67
Jan De Bleser <jandebleser@gmail.com>
2014-11-16 17:22:05 +0100

    fix for installing pycrypto

--- scripts/client/hudson.sh
+++ scripts/client/hudson.sh
@@ -2,7 +2,7 @@
 ps -ef | grep python | grep openport | grep -v grep | awk '{print $2}' | xargs -r kill
 cd scripts/client
 virtualenv env
-#sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount libffi-dev
+#sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount libffi-dev libssl-dev
 #mysql -uroot -e "grant all privileges on test_openport.* to 'hudson'@'localhost';" -p
 #env/bin/easy_install -U distribute
 #env/bin/easy_install unittest-xml-reporting
