311377bf23dea3f0171e592832cb2fd33ed19c5e
Jan De Bleser <jandebleser@gmail.com>
2014-11-19 15:08:53 +0100

    Small fix to let hudson run

--- scripts/client/hudson.sh
+++ scripts/client/hudson.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-ps -ef | grep python | grep openport | grep -v grep | awk '{print $2}' | xargs -r kill
-cd scripts/client
+ps -ef | grep python | grep openport | grep -v grep | awk '{print $2}' | xargs -r kill || echo 'nothing to kill'
+cd "$( cd "$( dirname "$0" )" && pwd )"
 virtualenv env
 #sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount libffi-dev libssl-dev
-#mysql -uroot -e "grant all privileges on test_openport.* to 'hudson'@'localhost';" -p
 #env/bin/easy_install -U distribute
 #env/bin/easy_install unittest-xml-reporting
 env/bin/pip install -r requirements.pip
 env/bin/pip install -r test-requirements.pip
 env/bin/nosetests --nocapture --with-xunit tests/*_tests.py || echo there was an error: $?
-./create_exes.sh
-ps -ef | grep python | grep openport | grep -v grep | awk '{print $2}' | xargs -r kill
+#./create_exes.sh
+ps -ef | grep python | grep openport | grep -v grep | awk '{print $2}' | xargs -r kill || echo 'nothing to kill'
