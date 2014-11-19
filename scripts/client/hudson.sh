d85d866a636a0cef4046872a24422c98309c8efd
Jan De Bleser <jandebleser@gmail.com>
2014-11-19 16:42:14 +0100

    python-virtualenv is required for the unittests

--- scripts/client/hudson.sh
+++ scripts/client/hudson.sh
@@ -2,7 +2,7 @@
 ps -ef | grep python | grep openport | grep -v grep | awk '{print $2}' | xargs -r kill || echo 'nothing to kill'
 cd "$( cd "$( dirname "$0" )" && pwd )"
 virtualenv env
-#sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount libffi-dev libssl-dev
+#sudo apt-get install libmysqlclient-dev python-dev nodejs sloccount libffi-dev libssl-dev python-virtualenv
 #env/bin/easy_install -U distribute
 #env/bin/easy_install unittest-xml-reporting
 env/bin/pip install -r requirements.pip
