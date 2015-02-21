7812eb1ee499a8f0f2a754bbe5a774969f2a6686
Jan De Bleser <jandebleser@gmail.com>
2015-02-21 18:25:09 +0100

    Passing the port a second parameter to the tryout script

--- scripts/client/tryouts/openthegate.sh
+++ scripts/client/tryouts/openthegate.sh
@@ -5,12 +5,12 @@ if [ $# -lt 1 ] ; then
 fi
 
 
-ip=46.137.72.214
-serverport=2022
+ip=openport.io
+serverport=$2
 localport=$1
 timeout=5000
 
-ssh -R *:$serverport:localhost:$localport open@$ip -n -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes sleep $timeout &
+ssh -R *:$serverport:localhost:$localport ubuntu@$ip -n -o StrictHostKeyChecking=no -o ExitOnForwardFailure=yes sleep $timeout &
 pid=$!
 
 echo "you are now connected, you can connect on $ip:$serverport"
