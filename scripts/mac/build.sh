2f61e4512620802cd8c6883c85835af907e3c247
Jan De Bleser <jandebleser@gmail.com>
2015-03-26 22:33:30 +0100

    Upped version and improved build scripts

--- scripts/mac/build.sh
+++ scripts/mac/build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+source ../client/apps/openport_app_version.py
+
 # get a mac developer licence.
 # in xcode, request a "Developer ID Installer" under Preferences>Accounts>View Details
 
 # for the summary image: make it 14 cm wide.
 
-packagesbuild -v packages/Openport.pkgproj
+sed "s/\\\$VERSION\\\$/$VERSION/g" packages/Openport.pkgproj > packages/Openport.pkgproj.tmp
 
-hdiutil create -volname Openport_0.9.1 -srcfolder packages/build -ov -format UDZO Openport_0.9.1.dmg
+packagesbuild -v packages/Openport.pkgproj.tmp
+
+hdiutil create -volname Openport_$VERSION -srcfolder packages/build -ov -format UDZO Openport_$VERSION.dmg
