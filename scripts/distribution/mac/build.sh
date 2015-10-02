3c7d68f59b3610f729d9bb450e7d4ddf70319507
Jan De Bleser (virtual mac) <jandebleser@gmail.com>
2015-10-02 11:25:23 +0200

    Fixed codesign in mac

--- scripts/distribution/mac/build.sh
+++ scripts/distribution/mac/build.sh
@@ -15,11 +15,10 @@ packagesbuild -v packages/Openport.pkgproj.tmp
 
 # Login your build user via the GUI and open Keychain Access. Select your signing private key, 
 # right-click, choose Get Info, change to the Access Control tab and select the "Allow all applications to access this item".
-# In addition, move your keys from "login" to "system".
 
 
 codesign --force --sign "Developer ID Application: Jan De Bleser" packages/build/Openport.pkg
 
 hdiutil create -volname Openport_$VERSION -srcfolder packages/build -ov -format UDZO Openport_$VERSION.dmg
 
-md5sum *.dmg > hash-mac.md5
+md5 -r *.dmg > hash-mac.md5
--- scripts/distribution/mac/compile_and_build_package.sh
+++ scripts/distribution/mac/compile_and_build_package.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+source local_settings.sh
 
 cd ../../client
 bash -ex ./create_exes_mac.sh
+
+
+security -v unlock-keychain -p $PASSWORD "/Users/jan/Library/Keychains/login.keychain"
 
 codesign --force --sign "Developer ID Application: Jan De Bleser" dist/openport
 codesign --force --sign "Developer ID Application: Jan De Bleser" dist/Openport.app
