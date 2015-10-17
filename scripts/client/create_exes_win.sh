7d00f9add71acdf3ad77c58429ed150fd8bdb6b0
Jan De Bleser <jandebleser@gmail.com>
2015-10-17 18:00:31 +0200

    Some extra information for creating the installer on windows

--- scripts/client/create_exes_win.sh
+++ scripts/client/create_exes_win.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-env/Scripts/pyinstaller --clean openport_win.spec -y
+env/Scripts/pyinstaller --clean openport.spec -y
 env/Scripts/pyinstaller --clean openport_win_no_console.spec -y
 env/Scripts/pyinstaller --clean openport-gui.spec -y
+
+./dist/openport/openport.exe --version
--- scripts/distribution/windows/create_installer.py
+++ scripts/distribution/windows/create_installer.py
@@ -25,10 +25,10 @@ signtool_path = "C:\\Program Files\\Microsoft SDKs\\Windows\\v6.0A\\bin\\signtoo
 # From 'file' add snap in 'certificates' for 'this computer'
 # From 'Certificates > more actions' click 'import'
 # Browse to the Danger Software.p12 certificate
-# Add it to 'Trusted Root Certification Authorities'
+# Add it to 'Trusted Root Certification Authorities' and to 'Personal'
 
 command = [signtool_path, 'sign',  '/sm', '/a', '/v', '/sm', '/s', 'My', '/n', 'Danger Software', 'openport_%s.exe' %VERSION]
-print command
+print '" "'.join(command)
 run_command(command)
 
 with open('hash-windows.md5', 'wb') as f:
