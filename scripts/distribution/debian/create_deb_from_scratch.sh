8f612f82ed620bc5be888c56258c2c1712a477ca
Jan De Bleser <jandebleser@gmail.com>
2016-10-06 12:01:22 +0200

    fixed build script

--- scripts/distribution/debian/create_deb_from_scratch.sh
+++ scripts/distribution/debian/create_deb_from_scratch.sh
@@ -4,8 +4,8 @@ set -ex
 export LD_LIBRARY_PATH=/usr/local/lib/python2.7.9/lib/
 
 git pull
-cd ../../client
-env/bin/pip install -r requirements.pip
-bash -ex ./create_exes.sh
+cd ../../openport
+virtualenv env
+env/bin/pip install -r requirements.txt
 cd ../distribution/debian
 bash -ex create_exes_and_deb.sh $1
