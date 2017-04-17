6e1aa5e848a8dcc80203ef995b32c5781b8a025d
Jan De Bleser <jandebleser@gmail.com>
2017-04-17 15:35:36 +0200

    Fixing issues with docker building

--- scripts/docker-create-exe.sh
+++ scripts/docker-create-exe.sh
@@ -5,5 +5,5 @@ set -x
 
 
 docker rm docker-openport -f || echo ""
-docker run -d --name docker-openport -v $(dirname $(dirname $(realpath $0))):/apps/openport/ jandebleser/openport-client2 
+docker run -d --name docker-openport -v $(dirname $(dirname $(dirname $(pwd)/$0))):/apps/openport/ jandebleser/openport-client2 
 docker exec -it docker-openport sudo -u docker ./scripts/create_exes.sh --no-gui 
