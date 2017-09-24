0170f636ddcebbb086afdef8c39847fc3c902fdb
Jan De Bleser <jandebleser@gmail.com>
2017-09-24 19:15:40 +0000

    Fixed issue with dockerfile on arm

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+MACHINE=$(uname -m)
+DOCKERARGS=
+if [ "$MACHINE" == "armv7l" ] ; then
+	DOCKERARGS="--build-arg FROMIMAGE=resin/rpi-raspbian:jessie"
+
+fi
 export DOCKER_API_VERSION=1.23
 
-docker build -t jandebleser/openport-client2 .
+docker build $DOCKERARGS -t jandebleser/openport-client2 .
 
