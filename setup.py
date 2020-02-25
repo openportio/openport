d0260c4520aeb07caacb9c1f8c67e2764bb5c434
Jan De Bleser <jandebleser@gmail.com>
2020-02-25 23:12:29 +0100

    Freezing docker image version

--- setup.py
+++ setup.py
@@ -19,12 +19,14 @@ install_reqs = [
     'six==1.10.0',
 ]
 
+
 def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
             paths.append(os.path.join('..', path, filename))
     return paths
+
 
 package_data = package_files('openport/alembic')
 
